b95dda2339530c2ce81acd69698e9df4cf39061c
shimaa <shimaafoda@std.mans.edu.eg>
2024-04-21 01:11:50 +0200

    commit

--- lib/features/auth/presentation/view/login.dart
+++ lib/features/auth/presentation/view/login.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const Scaffold(
-      body: Text("welcome"),
+      body: Text("welcome back"),
     );
   }
 }
